3a9111cc99d2d6435d5470c86fc00b7041e29daf
varma2423 <mudunuruvarma24@gmail.com>
2025-04-30 19:01:03 +0530

    fixed

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  basePath: '/admin',
+  // basePath: '/admin',
   output: 'export',
   images: {
     unoptimized: true, 
